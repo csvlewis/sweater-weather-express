bec7787107141f3815cd76580b256dae756f9d62
Chris Lewis <13chlewis@gmail.com>
2019-05-02 10:47:54 -0600

    Forecast requests now create an associated location in the database

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var User = require('../../../models').User;
+var Location = require('../../../models').Location;
 const fetch = require('node-fetch');
 pry = require('pryjs');
 
@@ -17,22 +18,33 @@ router.get('/', function(req, res, next){
       res.status(401).send(JSON.stringify("Invalid API key"));
     }
     else {
-      var search_location = req.url.split('=')[1].toLowerCase();
+      var search_location = req.query.location.toLowerCase();
       var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + search_location + '&key=AIzaSyDvKMInDzd7n_avQqlsflQwGxhllW-fhlM';
       fetch(url)
-      .then(function(response){
+      .then(response => {
         return response.json();
       })
-      .then(function(json){
+      .then(json => {
         var lat = json.results[0].geometry.location.lat;
         var lng = json.results[0].geometry.location.lng;
-        var url = 'https://api.darksky.net/forecast/' + '80ddbb9666791f550fbdf293adcd6bae/' + lat + ',' + lng;
-        fetch(url)
-        .then(function(response){
-          return response.json();
+        eval(pry.it)
+        Location.findOrCreate({
+          where: { name: search_location},
+          defaults: { latitude: lat, longitude: lng}
         })
-        .then(function(response){
-          res.status(200).send(response);
+        .then(location => {
+          var url = 'https://api.darksky.net/forecast/' + '80ddbb9666791f550fbdf293adcd6bae/' + lat + ',' + lng;
+          fetch(url)
+          .then(response => {
+            return response.json();
+          })
+          .then(response => {
+            res.status(200).send(response);
+          })
+          .catch(error => {
+            res.setHeader("Content-Type", "application/json");
+            res.status(500).send({ error })
+          });
         })
         .catch(error => {
           res.setHeader("Content-Type", "application/json");
